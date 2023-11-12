89643edaceee576c76fa213f95dd94e1f5726398
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-11-12 15:16:43 +0100

    Outputting Results in a List & Deriving More Value

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import UserInput from "./components/UserInput";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6, //%6
     duration: 10,
